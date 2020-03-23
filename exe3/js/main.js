0ff9ab700a8be91efeadf21039f7ec045453687d
MrJoo <phu.hitclub@gmail.com>
2020-03-23 23:27:14 +0700

    exe3: add radio + checkbox

--- exe3/js/main.js
+++ exe3/js/main.js
@@ -4,6 +4,7 @@ let issue = {
     description: 'task 1',
     type: 'task',
     priority: 'high',
+    allowFor: ['back_end', 'front_end', 'function_test', 'performance_test'],
     createdBy: 'Joo',
     assignee: 'joo',
     estimate: '20/03/2020',
@@ -11,21 +12,23 @@ let issue = {
 };
 
 const root = document.getElementById("root");
+const issueForm = document.issueForm;
 
 function showViewDetail() {
     let viewDetail = document.getElementsByClassName('view-detail')[0];
     viewDetail.style.display = "block";
-    document.getElementById('title').value = issue.title;
-    document.getElementById('description').value = issue.description;
-    document.getElementById('type').value =issue.type;
-    document.getElementById('priority').value = issue.priority;
-    document.getElementById('createdBy').value = issue.createdBy;
-    document.getElementById('assignee').value = issue.assignee;
-    document.getElementById('estimate').value = issue.estimate;
-    document.getElementById('state').value = issue.state;
+    issueForm.title.value = issue.title;
+    issueForm.description.value = issue.description;
+    issueForm.type.value = issue.type;
+    issueForm.priority.value = issue.priority;
+    issueForm.createdBy.value = issue.createdBy;
+    issueForm.assignee.value = issue.assignee;
+    issueForm.estimate.value = issue.estimate;
+    issueForm.state.value = issue.state;
+    setCheckboxValue();
 }
 
-function hideViewDetail(){
+function hideViewDetail() {
     let viewDetail = document.getElementsByClassName('view-detail')[0];
     viewDetail.style.display = "none";
 }
@@ -33,18 +36,40 @@ function hideViewDetail(){
 function showIssueInfo() {
     const pre = document.getElementsByTagName('pre')[0];
     pre.innerText = JSON.stringify(issue, null, 4);
-    console.log(issue)
 }
 
 function saveIssue() {
-    issue.title = document.getElementById('title').value;
-    issue.description = document.getElementById('description').value;
-    issue.type = document.getElementById('type').value;
-    issue.priority = document.getElementById('priority').value;
-    issue.createdBy = document.getElementById('createdBy').value;
-    issue.assignee = document.getElementById('assignee').value;
-    issue.estimate = document.getElementById('estimate').value;
-    issue.state = document.getElementById('state').value;
+    issue.title = issueForm.title.value;
+    issue.description = issueForm.description.value;
+    issue.type = issueForm.type.value;
+    issue.priority = issueForm.priority.value;
+    issue.createdBy = issueForm.createdBy.value;
+    issue.assignee = issueForm.assignee.value;
+    issue.estimate = issueForm.estimate.value;
+    issue.state = issueForm.state.value;
+    issue.allowFor = getCheckboxValue();
     hideViewDetail();
     showIssueInfo();
+}
+
+function getCheckboxValue() {
+    var ele = document.getElementsByName('allowFor');
+    let allowFor = [];
+    for (i = 0; i < ele.length; i++) {
+        if (ele[i].checked) {
+            allowFor.push(ele[i].value);
+        }
+    }
+    
+    return allowFor;
+}
+
+function setCheckboxValue() {
+    const allowFor = document.getElementsByName("allowFor");
+    for (i = 0; i < allowFor.length; i++) {
+        allowFor[i].checked = false;
+    }
+    for (i = 0; i < issue.allowFor.length; i++) {
+        document.getElementById(issue.allowFor[i]).checked = true;
+    }
 }
